9222cbcf958687842540348a16f4688a3d16b28f
Adam <adamicho123@gmail.com>
2022-08-17 18:43:04 +0200

    1.0

---
+++ Find The Anime.lua
@@ -0,0 +1,10 @@
+--By Foreverekk
+
+for i,v in ipairs(workspace.Fighters:GetDescendants()) do
+    if v.Name == "TouchInterest" and v.Parent then
+        firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 0)
+        wait(0.1)
+        firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 1)
+        game:service'Players'.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Parent.Position)
+    end
+end
