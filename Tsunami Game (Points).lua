d0774f0308878ae4e0485bac0b167c16de23f0ea
Adam <adamicho123@gmail.com>
2022-08-17 18:23:51 +0200

    1.0

---
+++ Tsunami Game (Points).lua
@@ -0,0 +1,9 @@
+--By Foreverekk
+
+for i,v in ipairs(workspace.CurrentPointCoins:GetDescendants()) do
+    if v.Name == "TouchInterest" and v.Parent then
+        firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 0)
+        wait(0.1)
+        firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 1)
+    end
+end
