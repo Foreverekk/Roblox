2ef5173ce9edb3924c959e781367559301a6c054
Adam <adamicho123@gmail.com>
2022-08-18 13:51:15 +0200

    1.0

---
+++ The VMA Experience.lua
@@ -0,0 +1,19 @@
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Credits";
+    Text = "Made by Foreverekk.#0007";
+    Duration = math.huge;
+    Button1 = "Ok"
+    })
+
+getgenv().Enabled = true
+
+while wait() and Enabled do
+    for i,v in ipairs(workspace.Scriptable.Tokens:GetDescendants()) do
+        if v.Parent and v.Parent.Name == "Token" then
+            wait(0.1)
+            game:service'Players'.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Position)
+        end
+    end
+    game:service'Players'.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(workspace.SpawnLocation.Position)
+    wait(1)
+end
