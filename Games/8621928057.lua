be8f57e235db9d1bd3bac12c08a9059b46dfc01d
Adam <adamicho123@gmail.com>
2022-08-19 22:02:28 +0200

    1.0

--- Games/8621928057.lua
+++ Games/8621928057.lua
@@ -1,8 +1,12 @@
 for i,v in ipairs(workspace.Pou:GetDescendants()) do
     if v.Name == "BadgeID" and v.Parent then
+        wait(0.1)
         game:service'Players'.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Parent.Position)
     end
 end
+
+wait(0.2)
+game:service'Players'.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(workspace.parts.SpawnLocation.Position)
 
 -- Codes:
 --  Rainbow Pou: 2536124
