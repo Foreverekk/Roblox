6e20e20358134b6b66c27a7d543acc5bf2407495
Adam <adamicho123@gmail.com>
2022-08-19 23:49:06 +0200

    1.0

--- Games/8003084678.lua
+++ Games/8003084678.lua
@@ -2,5 +2,11 @@ while wait(0.1) and Enabled do
     num = game.Players.LocalPlayer.leaderstats.Stage.Value + 1
     if workspace.Stages:FindFirstChild(tostring(num)) then
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Stages:FindFirstChild(tostring(num)).Spawn.CFrame
+    else
+        for i,v in ipairs(workspace:FindFirstChild("Restart Obby"):GetDescendants()) do
+            if v.Name == "TouchInterest" and v.Parent then
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Parent.CFrame
+            end
+        end
     end
 end
