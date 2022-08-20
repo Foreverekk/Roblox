88b022d4417b51ca60620c9f3cadb6a17f8275c6
Adam <adamicho123@gmail.com>
2022-08-21 00:47:44 +0200

    1.0

--- Games/9247388724.lua
+++ Games/9247388724.lua
@@ -2,7 +2,6 @@ function catsS()
     for i,v in ipairs(workspace:GetDescendants()) do
         if v.Name == "TouchInterest" and v.Parent and v.Parent.Name == "HumanoidRootPart" then
             firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 0)
-            wait(0.1)
             firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 1)
         end
     end
@@ -19,4 +18,3 @@ local S1 = T1:NewSection("Cats")
 S1:NewButton("Collect", "Collect all available cats", function()
     catsS()
 end)
---
