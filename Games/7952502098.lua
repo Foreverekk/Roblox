43b6a3445c390ff348ec4666c3e64d9758f6c4fb
Adam <adamicho123@gmail.com>
2022-08-20 23:39:10 +0200

    1.0

--- Games/7952502098.lua
+++ Games/7952502098.lua
@@ -1,4 +1,26 @@
-while wait(3) and Enabled do   
+function autofarmS()
+    wait(3)
     tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(0.7, Enum.EasingStyle.Linear)
-    tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(1105.77, 61.5607, 115.465)}):Play()
+    tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(1106, 61.5, 115.5)}):Play()
 end
+
+local T1 = Window:NewTab("Main")
+local S1 = T1:NewSection("AutoFarm")
+local autofarm = nil
+
+S1:NewToggle("Cash", "Autofarm cash", function(t)
+    autofarm = t
+    
+    while autofarm do
+        game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+        autofarmS()
+        game.Players.LocalPlayer.CharacterAdded:Connect(function()
+        game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+        if not autofarm then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+            return
+        end
+        autofarmS()
+    end)
+end
+end)
