d2ec8a573008e884b4527bbfd9d6f3a9a0e8a9a5
Adam <adamicho123@gmail.com>
2022-08-20 22:56:02 +0200

    1.0

--- Games/148763243.lua
+++ Games/148763243.lua
@@ -2,10 +2,15 @@ function badgesS()
     for i,v in ipairs(workspace:GetDescendants()) do
         if v.Name == "TouchInterest" and v.Parent then
             firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 0)
-            wait(0.1)
+            --wait(0.1)
             firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 1)
         end
     end
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "Collected!";
+        Text = "Made by Foreverekk.#0007";
+        Duration = math.Huge;
+    })
 end
 
 local T1 = Window:NewTab("Main")
