bf991ecd46da652e943f8d8abc7345cd4802d5a9
Adam <adamicho123@gmail.com>
2022-08-20 22:14:04 +0200

    1.0

--- Games/815405518.lua
+++ Games/815405518.lua
@@ -1,6 +1,33 @@
-while wait(0.5) and Enabled do
-    tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(0.7, Enum.EasingStyle.Linear)
-    tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-14.0395, 154.114, -15.0101)}):Play()
+function autofarmS()
     wait(0.5)
-    tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-16.2154, 164.597, -57.6469)}):Play()
+    game:service'Players'.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-14, 154, -15)
+    tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(1, Enum.EasingStyle.Linear)
+    local Tween1 = tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-14, 154, -15)}):Play()
+    Tween1:Play()
+    Tween1.Completed:Wait()
+    local Tween2 = tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-16, 164.5, -57.5)}):Play()
+    Tween2:Play()
+    Tween2.Completed:Wait()
 end
+
+local T1 = Window:NewTab("Main")
+local S1 = T1:NewSection("AutoFarm")
+local autofarm = nil
+
+S1:NewToggle("Survivals", "Autofarm survivals and points", function(t)
+    autofarm = t
+    
+    while autofarm do
+        game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+        autofarmS()
+        game.Players.LocalPlayer.CharacterAdded:Connect(function()
+        game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+        if not autofarm then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+            return
+        end
+        game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+        autofarmS()
+    end)
+end
+end)
