09fdebcc0eebc41b41acae6ca86406d6aee53635
Adam <adamicho123@gmail.com>
2022-08-20 22:34:30 +0200

    1.0

--- Games/3587619225.lua
+++ Games/3587619225.lua
@@ -1,7 +1,43 @@
-while wait(0.1) and Enabled do
-    game:GetService("ReplicatedStorage").Events.prestige:FireServer()
+function completeS()
+    wait(0.1)
     num = game.Players.LocalPlayer.leaderstats.Stage.Value + 1
     if workspace.Checkpoints:FindFirstChild(tostring(num)) then
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Checkpoints:FindFirstChild(tostring(num)).CFrame
     end
 end
+
+function prestigeS()
+    wait(0.1)
+    game:GetService("ReplicatedStorage").Events.prestige:FireServer()
+end
+
+local T1 = Window:NewTab("Main")
+local S1 = T1:NewSection("AutoFarm")
+local complete = nil
+local prestige = nil
+
+S1:NewToggle("Complete", "Autofarm stages", function(t)
+    complete = t
+    
+    while complete do
+        completeS()
+        game.Players.LocalPlayer.CharacterAdded:Connect(function()
+        game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+        if not complete then return end
+        completeS()
+    end)
+end
+end)
+
+S1:NewToggle("Prestige", "Autofarm prestige", function(t)
+    prestige = t
+    
+    while prestige do
+        prestigeS()
+        game.Players.LocalPlayer.CharacterAdded:Connect(function()
+        game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+        if not prestige then return end
+        prestigeS()
+    end)
+end
+end)
