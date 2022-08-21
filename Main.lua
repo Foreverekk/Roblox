05825e67b2778393386e35c5908f3b3c0d9ced7e
Adam <adamicho123@gmail.com>
2022-08-21 20:13:13 +0200

    1.0

--- Main.lua
+++ Main.lua
@@ -1,5 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Foreverekk/Roblox/main/Loading.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Foreverekk/Roblox/main/Input.lua"))()
 
 loaded = false
 
