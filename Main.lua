25e1713dfeb6397a88e6d9776b6d20f8abdc16ae
Adam <adamicho123@gmail.com>
2022-08-21 20:14:45 +0200

    1.0

--- Main.lua
+++ Main.lua
@@ -30,8 +30,8 @@ if _G.KeyInput == _G.Key then
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Foreverekk/Roblox/main/Key.lua"))()
     KeyLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-    KeyWindow = Library.CreateLib("Enter Key | FRV Hub", "DarkTheme")
-    local Key = Window:NewTab("Unlock")
+    KeyWindow = KeyLibrary.CreateLib("Enter Key | FRV Hub", "DarkTheme")
+    local Key = KeyWindow:NewTab("Unlock")
     local K1 = Key:NewSection("discord.gg/RetmyBsNAV")
     K1:NewTextBox("Enter key here", "Join Discord for a key", function(c)
         _G.KeyInput = c
