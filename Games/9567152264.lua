33c90edfd802c019bd5490a0837008ef8aa77e32
Adam <adamicho123@gmail.com>
2022-08-21 16:11:51 +0200

    1.0

--- Games/9567152264.lua
+++ Games/9567152264.lua
@@ -1,9 +1,22 @@
 function scpS()
     for i,v in ipairs(workspace.SCPs:GetDescendants()) do
-        if v.Name == "TouchInterest" and v.Parent then
+        if v.Name == "TouchInterest" and v.Parent and v.Parent.ClassName == "Part" then
             firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 0)
-            wait(0.1)
             firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 1)
+        end
+    end
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "Collected!";
+        Text = "Made by Foreverekk.#0007";
+        Duration = 60;
+    })
+end
+
+function scptpS()
+    for i,v in ipairs(workspace.SCPs:GetDescendants()) do
+        if v.Name == "TouchInterest" and v.Parent and v.Parent.ClassName == "Part" then
+            wait(0.1)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Parent.CFrame
         end
     end
     game.StarterGui:SetCore("SendNotification", {
@@ -18,4 +31,8 @@ local S1 = T1:NewSection("SCP")
 
 S1:NewButton("Collect", "Collect all available SCP's", function()
     scpS()
+end)
+
+S1:NewButton("Collect (TP)", "Collect all available SCP's", function()
+    scptpS()
 end)
