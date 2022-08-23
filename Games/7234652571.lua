4692ecbaea956e2c34d921e7a0f329c5e18e1112
Adam <adamicho123@gmail.com>
2022-08-23 03:16:52 +0200

    1.0

---
+++ Games/7234652571.lua
@@ -0,0 +1,25 @@
+function cratesS()
+    for i,v in ipairs(workspace.CrateParent:GetDescendants()) do
+        if v.Name == "TouchInterest" and v.Parent and v.Parent.Name == "CashCrate" then
+            firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 0)
+            firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 1)
+        end
+    end
+end
+
+local T1 = Window:NewTab("Main")
+local S1 = T1:NewSection("Autofarm")
+local crates = nil
+
+S1:NewToggle("Crates", "Autofarm crates", function(c)
+    crates = c
+    
+    while crates do
+        cratesS()
+        game.Players.LocalPlayer.CharacterAdded:Connect(function()
+        game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+        if not crates then return end
+        cratesS()
+    end)
+end
+end)
