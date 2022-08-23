df931c5c395ff33d7bad57c3ed496cab217338b0
Adam <adamicho123@gmail.com>
2022-08-23 03:17:24 +0200

    1.0

--- Games/7234652571.lua
+++ Games/7234652571.lua
@@ -14,7 +14,7 @@ local crates = nil
 S1:NewToggle("Crates", "Autofarm crates", function(c)
     crates = c
     
-    while crates and wait(1) do
+    while crates and wait(2) do
         cratesS()
         game.Players.LocalPlayer.CharacterAdded:Connect(function()
         game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
