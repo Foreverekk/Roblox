732b5d645e9d705910f67137dc1b4804b6f7ecad
Adam <adamicho123@gmail.com>
2022-08-23 21:15:04 +0200

    1.0

--- Games/6490641023.lua
+++ Games/6490641023.lua
@@ -1,4 +1,4 @@
-function completeS()
+function stagesS()
     num = game.Players.LocalPlayer.leaderstats.Stage.Value + 1
     if workspace.Stages:FindFirstChild(tostring(num)) then
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Stages:FindFirstChild(tostring(num)).Spawn.CFrame
@@ -7,17 +7,17 @@ end
 
 local T1 = Window:NewTab("Main")
 local S1 = T1:NewSection("AutoFarm")
-local complete = nil
+local stages = nil
 
-S1:NewToggle("Complete", "Autofarm stages", function(c)
-    complete = c
+S1:NewToggle("Stages", "Autofarm stages", function(c)
+    stages = c
     
-    while complete and wait(0.1) do
-        completeS()
+    while stages and wait(0.1) do
+        stagesS()
         game.Players.LocalPlayer.CharacterAdded:Connect(function()
         game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
-        if not complete then return end
-        completeS()
+        if not stages then return end
+        stagesS()
     end)
 end
 end)
