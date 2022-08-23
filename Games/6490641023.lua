19956bf869eea5f7677c4799a768a7da58657849
Adam <adamicho123@gmail.com>
2022-08-23 21:26:14 +0200

    1.0

--- Games/6490641023.lua
+++ Games/6490641023.lua
@@ -6,9 +6,25 @@ function stagesS()
     end
 end
 
+function prestigeS()
+    num = game.Players.LocalPlayer.leaderstats.Stage.Value + 1
+    if workspace.Stages:FindFirstChild(tostring(num)) then
+        return
+    else
+        for i,v in ipairs(workspace:FindFirstChild("Play Now!"):GetDescendants()) do
+            if v.Name == "TouchInterest" and v.Parent then
+                firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 0)
+                firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 1)
+            end
+        end
+    end
+end
+
+
 local T1 = Window:NewTab("Main")
 local S1 = T1:NewSection("AutoFarm")
 local stages = nil
+local prestige = nil
 
 S1:NewToggle("Stages", "Autofarm stages", function(c)
     stages = c
@@ -19,6 +35,19 @@ S1:NewToggle("Stages", "Autofarm stages", function(c)
         game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
         if not stages then return end
         stagesS()
+    end)
+end
+end)
+
+S1:NewToggle("Prestige", "Autofarm prestige", function(c)
+    prestige = c
+    
+    while prestige and wait() do
+        prestigeS()
+        game.Players.LocalPlayer.CharacterAdded:Connect(function()
+        game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+        if not prestige then return end
+        prestigeS()
     end)
 end
 end)
