73db3b682dde57dae0fe3e12fcc71c6cb7df40a9
Adam <adamicho123@gmail.com>
2022-08-23 03:23:34 +0200

    1.0

--- Games/7234652571.lua
+++ Games/7234652571.lua
@@ -2,6 +2,7 @@ function cratesS()
     for i,v in ipairs(workspace.CrateParent:GetDescendants()) do
         if v.Name == "TouchInterest" and v.Parent and v.Parent.Name == "CashCrate" then
             firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 0)
+            wait(0.1)
             firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 1)
         end
     end
