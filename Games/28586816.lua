20eb938fc515656c03a9e906c6a86d9dada73d48
Adam <adamicho123@gmail.com>
2022-08-25 17:36:05 +0200

    1.0

--- Games/28586816.lua
+++ Games/28586816.lua
@@ -1,7 +1,4 @@
 function pointsS()
-    --if game:GetService("Players")["LocalPlayer"].Character.AlreadyGotXP then
-    --    game:GetService("Players")["LocalPlayer"].Character.AlreadyGotXP:Destroy()
-    --end
     for i,v in ipairs(workspace.XPDrops:GetDescendants()) do
         if v.Name == "TouchInterest" and v.Parent then
             firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 0)
@@ -9,9 +6,6 @@ function pointsS()
         end
     end
     tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(1, Enum.EasingStyle.Linear)
-    --local Tween0 = tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = workspace.XPDrops.XP.Center.CFrame})
-    --Tween0:Play()
-    --Tween0.Completed:Wait()
     local Tween1 = tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-266.79, 677.628, -398.15)})
     Tween1:Play()
     Tween1.Completed:Wait()
@@ -25,7 +19,7 @@ function pointsS()
     local Tween4 = tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-265.588, 142.915, -2350.8)})
     Tween4:Play()
     Tween4.Completed:Wait()
-    wait(10)
+    wait(5)
     game.Players.LocalPlayer.Character.Humanoid.Health = 0
 end
 
