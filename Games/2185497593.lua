a5c724c991b68ce7ba206e9a92252874697d29e7
Adam <adamicho123@gmail.com>
2022-09-10 15:40:54 +0200

    1.0

--- Games/2185497593.lua
+++ Games/2185497593.lua
@@ -1,6 +1,6 @@
 function moneyS()
     for i,v in ipairs(workspace.Chips:GetDescendants()) do
-        if v.Name == "TouchInterest" and v.Parent and v.Parent.Name == "Chip" then
+        if v and v.Name == "TouchInterest" and v.Parent and v.Parent.Name == "Chip" then
             firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 0)
             wait(0.1)
             firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 1)
