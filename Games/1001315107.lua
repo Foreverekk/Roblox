7affb0e06af3f848fc374bea70af1d5a4bfe94c6
Adam <adamicho123@gmail.com>
2022-10-05 11:46:13 +0200

    2.0

--- Games/1001315107.lua
+++ Games/1001315107.lua
@@ -1,5 +1,7 @@
 local T1 = Window:NewTab("Main")
 local S1 = T1:NewSection("Armory")
+local S0 = T1:NewSection("Premium")
+local S2 = T1:NewSection("Badges")
 
 function batS()
     loadstring(game:HttpGet(mainRaw.."Scripts/"..game.PlaceId.."-bat.lua"))()
@@ -17,6 +19,10 @@ function armorS()
     loadstring(game:HttpGet(mainRaw.."Scripts/"..game.PlaceId.."-armor.lua"))()
 end
 
+function badgesS()
+    loadstring(game:HttpGet(mainRaw.."Scripts/"..game.PlaceId.."-badges.lua"))()
+end
+
 S1:NewButton("Bat/Golden Bat", "Will try to give you weapons", function()
     batS()
 end)
@@ -31,4 +37,8 @@ end)
 
 S1:NewButton("Armor", "Will try to give you armor", function()
     armorS()
+end)
+
+S2:NewButton("MEET THE OWNER", "Collect", function()
+    badgesS()
 end)
