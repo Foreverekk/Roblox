e196e145d97493785d4f8d40ad85fec372dd486f
Adam <adamicho123@gmail.com>
2023-02-16 21:27:47 +0100

    0

--- V2/Main.lua
+++ V2/Main.lua
@@ -29,21 +29,9 @@ local Window = Library:CreateWindow("FRV Hub V2")
 local Game = Window:AddFolder("Game")
 
 local Global = Window:AddFolder("Global")
-Global:AddToggle({ text = "Toggle", flag = "toggle", state = false, callback = function(a) print(a) end })
-Global:AddToggle({ text = "Toggle1", flag = "toggle1", state = true, callback = function(a) print(a) end })
-
-Global:AddToggle({ text = "Toggle2", flag = "toggle2", state = false, callback = function(a) print(a) end})
-Global:AddToggle({ text = "Toggle3", flag = "toggle3", state = true, callback = function(a) print(a) end})
-
-Global:AddList({ text = "List", flag = "list", value = "Value", values = {"Value1", "Value2", "Value3", "Value4"}, callback = function(a) print(a) end })
-Global:AddList({ text = "List1", flag = "list1", value = "Value2", values = {"Value1", "Value2", "Value3", "Value4"}, callback = function(a) print(a) end })
-
-Global:AddBox({ text = "Box", flag = "box", value = "Value", callback = function(a) print(a) end })
-Global:AddBox({ text = "Box1", flag = "box1", value = "Value", callback = function(a) print(a) end })
-
-Global:AddLabel({ text = "Player" })
-Global:AddSlider({ text = "WalkSpeed", flag = "sliderWalkSpeed", value = 16, min = 0, max = 500, float = 0.5, callback = function(a) game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = a end })
-Global:AddSlider({ text = "JumpPower", flag = "sliderJumpPower", value = 50, min = 0, max = 1000, float = 0.5, callback = function(a) game.Players.LocalPlayer.Character.Humanoid.JumpPower = a end })
+Global:AddLabel({ text = "[Global] Player" })
+Global:AddSlider({ text = "WalkSpeed", flag = "sliderWalkSpeed", value = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed, min = 0, max = 500, float = 0.5, callback = function(a) game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = a end })
+Global:AddSlider({ text = "JumpPower", flag = "sliderJumpPower", value = game.Players.LocalPlayer.Character.Humanoid.JumpPower, min = 0, max = 1000, float = 0.5, callback = function(a) game.Players.LocalPlayer.Character.Humanoid.JumpPower = a end })
 Global:AddList({ text = "Camera Mode", flag = "listCameraMode", value = "Classic", values = {"Classic", "LockFirstPerson"}, callback = function(a)
     if a == "Classic" then
         game:GetService("Players")["LocalPlayer"].CameraMode = Enum.CameraMode.Classic
@@ -51,16 +39,11 @@ Global:AddList({ text = "Camera Mode", flag = "listCameraMode", value = "Classic
         game:GetService("Players")["LocalPlayer"].CameraMode = Enum.CameraMode.LockFirstPerson
     end
 end })
-
-Global:AddLabel({ text = "Toggles" })
-Global:AddToggle({ text = "AutoJump", flag = "toggleAutoJumpEnabled", state = false, callback = function(a) 
-    if a == true then
-        game:GetService("Players")["LocalPlayer"].AutoJumpEnabled = true
-    else
-        game:GetService("Players")["LocalPlayer"].AutoJumpEnabled = false
-    end
-
+Global:AddBox({ text = "Display Name", flag = "boxDisplayName", value = game.Players.LocalPlayer.DisplayName, callback = function(a)
+    game.Players.LocalPlayer.DisplayName = a
 end })
+
+Global:AddLabel({ text = "[Global] Toggles" })
 Global:AddButton({ text = "AutoScaling", flag = "buttonAutomaticScalingEnabled", callback = function() 
     if game:GetService("Players")["LocalPlayer"].Character.Humanoid.AutomaticScalingEnabled == true then
         game:GetService("Players")["LocalPlayer"].Character.Humanoid.AutomaticScalingEnabled = false
@@ -101,18 +84,26 @@ Global:AddToggle({ text = "Neutral (Team)", flag = "toggleNeutral", state = fals
     end
     
 end })
+Global:AddToggle({ text = "AutoJump", flag = "toggleAutoJumpEnabled", state = false, callback = function(a) 
+    if a == true then
+        game:GetService("Players")["LocalPlayer"].AutoJumpEnabled = true
+    else
+        game:GetService("Players")["LocalPlayer"].AutoJumpEnabled = false
+    end
 
-Global:AddLabel({ text = "Experimental" })
+end })
+
+Global:AddLabel({ text = "[Global] Experimental" })
 Global:AddSlider({ text = "Hitbox Height", flag = "sliderHipHeight", value = 2.72, min = 2.72, max = 100, float = 0.1, callback = function(a) game.Players.LocalPlayer.Character.Humanoid.HipHeight = a end })
 
 local Settings = Window:AddFolder("Settings")
-Settings:AddLabel({ text = "Bind" })
+Settings:AddLabel({ text = "[Settings] Bind" })
 Settings:AddBind({ text = "Bind", flag = "bind", key = "MouseButton1", callback = function() print("pressed") end }) -- key can also be Enum.UserInputType.MouseButton1, instead of the name/string
 Settings:AddBind({ text = "Bind", flag = "bind", hold = true, key = "E" , callback = function(a) if a then print("let go") else print("holding") end end })
 Settings:AddBind({ text = "Toggle UI", key = "PageUp", callback = function() library:Close() end })
 
 local Info = Window:AddFolder("Info")
-Info:AddLabel({ text = "Player" })
+Info:AddLabel({ text = "[Info] Player" })
 Info:AddButton({ text = "Name: "..game.Players.LocalPlayer.Name, flag = "buttonCopy0", callback = function() setclipboard(game.Players.LocalPlayer.Name) end })
 Info:AddButton({ text = "ID: "..game.Players.LocalPlayer.UserId, flag = "buttonCopy1", callback = function() setclipboard(game.Players.LocalPlayer.UserId) end })
 Info:AddButton({ text = "Age: "..game.Players.LocalPlayer.AccountAge.." days", flag = "buttonCopy2", callback = function() setclipboard(game.Players.LocalPlayer.AccountAge.." days") end })
@@ -122,8 +113,8 @@ Info:AddColor({ text = "Color", flag = "color", color = {1, 0.2, 0.2}, callback 
 local Premium = Window:AddFolder("Premium")
 
 local Credits = Window:AddFolder("Credits")
-Credits:AddLabel({ text = "Scripts: Foreverekk.#0007" })
-Credits:AddLabel({ text = "UI: alixjaffar" })
+Credits:AddLabel({ text = "[Scripts] Foreverekk.#0007" })
+Credits:AddLabel({ text = "[UI] alixjaffar" })
 
 Library:Init()
  
