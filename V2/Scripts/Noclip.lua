66517205a1dc2e51cd9f529be3de80b2d0aee8c4
Adam <adamicho123@gmail.com>
2023-03-26 22:33:50 +0200

    2.1.2

--- V2/Scripts/Noclip.lua
+++ V2/Scripts/Noclip.lua
@@ -2,53 +2,53 @@ local player = game.Players.LocalPlayer
 local character = player.Character
 local humanoid = character:WaitForChild("Humanoid")
 local camera = workspace.CurrentCamera
-local walking = false
+_G.Walking = false
 local UserInputService = game:GetService("UserInputService")
 
 toggleNoclip = function()
-	if walking == false then
-		walking = true
-        character:WaitForChild("HumanoidRootPart").Anchored = true
-        character:WaitForChild("HumanoidRootPart").CanCollide = false
-		camera.CFrame = character:WaitForChild("HumanoidRootPart").CFrame
-		camera.CameraType = "Custom"
+	if _G.Walking == false then
+		_G.Walking = true
+        game:GetService("Players")["LocalPlayer"].Character:WaitForChild("HumanoidRootPart").Anchored = true
+        game:GetService("Players")["LocalPlayer"].Character:WaitForChild("HumanoidRootPart").CanCollide = false
+		game:GetService("Workspace").CurrentCamera.CFrame = character:WaitForChild("HumanoidRootPart").CFrame
+		game:GetService("Workspace").CurrentCamera.CameraType = "Custom"
 	else
-		walking = false
-        character:WaitForChild("HumanoidRootPart").Anchored = false
-        character:WaitForChild("HumanoidRootPart").CanCollide = true
-		camera.CFrame = character:WaitForChild("HumanoidRootPart").CFrame
-		camera.CameraType = "Follow"
+		_G.Walking = false
+        game:GetService("Players")["LocalPlayer"].Character:WaitForChild("HumanoidRootPart").Anchored = false
+        game:GetService("Players")["LocalPlayer"].Character:WaitForChild("HumanoidRootPart").CanCollide = true
+		game:GetService("Workspace").CurrentCamera.CFrame = character:WaitForChild("HumanoidRootPart").CFrame
+		game:GetService("Workspace").CurrentCamera.CameraType = "Follow"
 	end
 end
 
 local function Fly()
-    if UserInputService:IsKeyDown(Enum.KeyCode.W) and walking == true then
+    if UserInputService:IsKeyDown(Enum.KeyCode.W) and _G.Walking == true then
         camera.CFrame = camera.CFrame + camera.CFrame.lookVector * 10
         character:WaitForChild("HumanoidRootPart").CFrame = character:WaitForChild("HumanoidRootPart").CFrame + Vector3.new(0, 0, 1)
     end
-    if UserInputService:IsKeyDown(Enum.KeyCode.S) and walking == true then
+    if UserInputService:IsKeyDown(Enum.KeyCode.S) and _G.Walking == true then
         camera.CFrame = camera.CFrame - camera.CFrame.lookVector * 10
         character:WaitForChild("HumanoidRootPart").CFrame = character:WaitForChild("HumanoidRootPart").CFrame + Vector3.new(0, 0, -1)
     end
-    if UserInputService:IsKeyDown(Enum.KeyCode.A) and walking == true then
+    if UserInputService:IsKeyDown(Enum.KeyCode.A) and _G.Walking == true then
         camera.CFrame = camera.CFrame * CFrame.new(0, 0, 10)
         character:WaitForChild("HumanoidRootPart").CFrame = character:WaitForChild("HumanoidRootPart").CFrame + Vector3.new(-1, 0, 0)
     end
-    if UserInputService:IsKeyDown(Enum.KeyCode.D) and walking == true then
+    if UserInputService:IsKeyDown(Enum.KeyCode.D) and _G.Walking == true then
         camera.CFrame = camera.CFrame * CFrame.new(0, 0, -10)
         character:WaitForChild("HumanoidRootPart").CFrame = character:WaitForChild("HumanoidRootPart").CFrame + Vector3.new(1, 0, 0)
     end
-    if UserInputService:IsKeyDown(Enum.KeyCode.Space) and walking == true then
+    if UserInputService:IsKeyDown(Enum.KeyCode.Space) and _G.Walking == true then
         camera.CFrame = camera.CFrame * CFrame.new(0, 10, 0)
         character:WaitForChild("HumanoidRootPart").CFrame = character:WaitForChild("HumanoidRootPart").CFrame + Vector3.new(0, 1, 0)
     end
-    if UserInputService:IsKeyDown(Enum.KeyCode.LeftControl) and walking == true then
+    if UserInputService:IsKeyDown(Enum.KeyCode.LeftControl) and _G.Walking == true then
         camera.CFrame = camera.CFrame * CFrame.new(0, -10, 0)
         character:WaitForChild("HumanoidRootPart").CFrame = character:WaitForChild("HumanoidRootPart").CFrame + Vector3.new(0, -1, 0)
     end
 end
 
-while walking == true do
+while _G.Scripts and _G.Walking == true do
 	Fly()
 	wait(0.01)
 end
